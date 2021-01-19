1913bbb66e60b4d414e2574bee65c7d5d4368cdd
enaburekhan <ericenaburekhan2018@gmail.com>
2021-01-19 16:23:07 +0100

    create and add markup to pageloader.js

---
+++ src/pageloader.js
@@ -0,0 +1,28 @@
+const pageloader = (() => {
+//container
+ 
+  const dataContainer = document.querySelector('#content');
+
+  const header = document.createElement('header')
+  header.innerHTML = `<nav class="nav-bar">
+  <div class="food">
+    <p>Healthy Food</p>
+    
+  </div>
+  <ul class="nav-home">
+    <li>
+        <a href="#home" class="">Home</a>
+    </li>
+    <li>
+        <a href="#menu" class="">Menu</a>
+    </li>
+    <li>
+        <a href="#contact" class="">Contact us</a>
+    </li>   
+  </ul>
+</nav>`;
+
+dataContainer.appendChild(header);
+
+
+})();

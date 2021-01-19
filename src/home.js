914f19e965f3eef8252fce78df8c2a04dc5bcd6f
enaburekhan <ericenaburekhan2018@gmail.com>
2021-01-19 15:41:55 +0100

    create home.js and menu.js and add their respective markup

---
+++ src/home.js
@@ -0,0 +1,35 @@
+const displayHomeSection = (() => {
+//container
+const dataContainer = document.querySelector('#content'); 
+
+const homeSection = document.createElement('div');
+
+homeSection.classList.add('tab-content');
+
+homeSection.innerHTML = `<div class="home-section active data-tab-content">
+<div class="make-order">
+    <h1>Make your order within Lagos, and we deliver to your doorstep</h1>
+    <p>We take pride in making affordable healthy foods for a healthy society</p>
+    <div class="btn-container">
+     <a data-tab-target="#menu" class="order-now">Make Your Order</a>
+   </div>
+</div>
+<div class="location">
+  <div class="address">
+    <span><i class='fas fa-map-marker-alt'></i></span>
+    <p>plot 17, Glory land Estate, Winners avenue<br />Egbeda, Lagos</p>
+  </div>
+  <div class="work-hours">
+    <span><i class='far fa-clock'></i></span>
+    <p>
+      <span>Mon-Fri:</span>8am-8pm<br /><span>Fri-Sun:</span
+      >9am-10pm
+    </p>
+  </div>
+</div>
+</div>`;
+
+dataContainer.appendChild(homeSection);
+})();
+
+export { displayHomeSection };
---
+++ src/menu.js
@@ -0,0 +1,61 @@
+const displayMenuSection = (() => {
+  //container
+  const dataContainer = document.querySelector('#content');
+  const menuSection = document.createElement('div');
+  menuSection.setAttribute('id', 'menu');
+  menuSection.setAttribute('data-tab-content');
+  menuSection.innerHTML = `<div class="header">
+  <h1>Menu</h1>
+  <img src="./img/chef.png" alt="" width="50" height="50" />
+</div>
+
+<div class="menu-section">
+  <div class="menu-item">
+      <img src="./img/sandwich1.jpg" alt="sandwich" />
+      <div class="item-content">
+        <h4>Sandwich: <span>#1k</span></h4>
+        <p>
+          Buns, patty, tomato, onions, lettuce, and our secret family
+          recipe.
+        </p>
+      </div>
+  </div>
+  <div class="menu-item">
+    <img src="./img/sandwich2.jpg" alt="sandwich" />
+    <div class="item-content">
+      <h4>Yommy Sandwich: <span>#1.2k</span></h4>
+      <p>
+        Buns, patty, tomato, onions, lettuce, and our secret family
+        recipe reloaded++.
+      </p>
+    </div>
+  </div> 
+  <div class="menu-item">
+    <img src="./img/salad.jpg" alt="salad" />
+    <div class="item-content">
+      <h4>Salad: <span>#2.5k</span></h4>
+      <p>
+        lettuce, spinach, Baked Tortilla, fresh fruits, Beans and Legumes, and our secret family
+        recipe.
+      </p>
+    </div>
+  </div> 
+  <div class="menu-item">
+    <img src="./img/coffee.jpg" alt="coffee" />
+    <div class="item-content">
+      <h4>Caffè Latte: <span>#0.7k</span></h4>
+      <p>
+        espresso coffee ground, milk, sugar, and our secret family
+        recipe.
+      </p>
+    </div>
+  </div>      
+
+</div>`;
+
+dataContainer.appendChild(menuSection);
+
+
+})();
+
+export { displayMenuSection };
